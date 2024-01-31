7ea2e6ce2ba2745dfb1de5e7e65b5717c3a910bd
igor-primo <igorprimo62@gmail.com>
2024-01-31 20:26:58 -0300

    new start

--- .source/pv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PATH="/home/igor/perl5/bin${PATH:+:${PATH}}"; export PATH;
-PERL5LIB="/home/igor/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="/home/igor/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"/home/igor/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/home/igor/perl5"; export PERL_MM_OPT;
---
+++ dotfiles-fetch.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+
+# Programs:
+# dunst
+# 2bwm
+# emacs
+# rofi
+# picom
+
+_destfile="./dotfiles-$(date +%F).tar"
+
+# Configs
+_arr=("/home/$USER/.config/dunst/dunstrc")
+_arr+=("/home/$USER/.config/rofi/config.rasi")
+_arr+=("/home/$USER/repos/2bwm/config.h")
+_arr+=("/home/$USER/.picom.conf")
+_arr+=("/home/$USER/.emacs.d/init.el")
+_arr+=("/home/$USER/.xinitrc")
+_arr+=("/home/$USER/.Xresources")
+
+# Bins
+_arr+=("/home/$USER/bg.jpg")
+
+# Scripts
+_arr+=($(find /home/$USER/scripts))
+
+tar cvPf $_destfile ${_arr[@]}
