44daa310d7b45f34bcb9497302fbd4180898829c
igor-primo <igorprimo62@gmail.com>
2024-02-01 11:33:18 -0300

    atualização

--- dotfiles-fetch.sh
+++ dotfiles-fetch.sh
@@ -24,4 +24,4 @@ _arr+=("/home/$USER/bg.jpg")
 # Scripts
 _arr+=($(find /home/$USER/scripts))
 
-tar cvPf $_destfile ${_arr[@]}
+tar cvPf $_destfile ${_arr[@]} && git commit -a -m 'atualização' && git push
