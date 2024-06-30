4b931bc6a13c5356e3aa88961e8180cc350c6731
igor-primo <igorprimo62@gmail.com>
2024-06-30 10:48:35 -0300

    first flake attempt

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations."igor-nix" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home.manager.useGlobalPkgs = true;
+          home-manager.users.igor = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
