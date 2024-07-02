3af43f469a6adbd2db90e939749248c26cb35f59
igor-primo <igorprimo62@gmail.com>
2024-07-02 00:52:45 -0300

    add some el config for latex and xmonad layouts

--- dotfiles/xmonad.hs
+++ dotfiles/xmonad.hs
@@ -2,15 +2,13 @@
 
 import XMonad
 
-import Data.Ratio
-
 import XMonad.Util.EZConfig
-import XMonad.Util.Ungrab
 import XMonad.Layout.Gaps
 import XMonad.Layout.Spacing
-import XMonad.Layout.Spiral
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.MultiColumns
+import XMonad.Layout.TwoPane
 import qualified XMonad.Util.Hacks as Hacks
 
 main :: IO ()
@@ -21,7 +19,7 @@ main = do
           , borderWidth = 3
           , normalBorderColor = "#4f3928"
           , focusedBorderColor = "#ebe9e7"
-          , layoutHook = spacingWithEdge 5 $ gaps [(U, 5)] $ Full |||  ResizableTall 1 (3/100) (1/2) [] ||| ThreeColMid 1 (3/100) (1/2) ||| ThreeCol 1 (3/100) (1/2)
+          , layoutHook = spacingWithEdge 5 $ gaps [(U, 5)] $ Full ||| TwoPane (3/100) (1/2) ||| ResizableTall 1 (3/100) (1/2) [] ||| ThreeColMid 1 (3/100) (1/2) ||| ThreeCol 1 (3/100) (1/2)
           , handleEventHook = handleEventHook def <> Hacks.windowedFullscreenFixEventHook
           }
           `additionalKeysP`
