3226d6859b25e8b00fe2209cd12c5d9f88eb4e56
igor-primo <igorprimo62@gmail.com>
2024-08-28 23:37:45 -0300

    clean home.nix from useless comments

--- home.nix
+++ home.nix
@@ -1,30 +1,11 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "igor";
   home.homeDirectory = "/home/igor";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-
+  home.stateVersion = "24.05";
 
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    #pkgs.base16
     pkgs.fzf
     pkgs.picom
     pkgs.zsh
@@ -49,11 +30,6 @@
     pkgs.alacritty
     pkgs.kitty
     pkgs.tor-browser-bundle-bin
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
     (pkgs.nerdfonts.override { fonts = [ "Inconsolata" ]; })
 
     (pkgs.st.overrideAttrs (oldAttrs: rec {
@@ -61,14 +37,6 @@
       postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
     }))
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-
-    #(builtins.getFlake "github:igor-primo/nixvim-elegant").packages.x86_64-linux.default
     (builtins.getFlake "path:/home/igor/repos/nixvim").packages.x86_64-linux.default
   ];
 
@@ -94,14 +62,7 @@
 
   imports = [ ./zsh.nix ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
     ".tmux.conf".source = dotfiles/tmux.conf;
     ".xmonad/xmonad.hs".source = dotfiles/xmonad.hs;
     ".base16.env".source = dotfiles/base16.env;
@@ -131,34 +92,11 @@
     ".scripts/kitty.sh".source = dotfiles/kitty.sh;
 
     "bg.png".source = dotfiles/bg.png;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/igor/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "vim";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
